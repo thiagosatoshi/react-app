458ad4b7818c552ec2e7499433d7b5efbe577e66
Thiago Suzuki <TSU05@bskyb.com>
2017-10-11 20:35:26 +0100

    Added checkbox component to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Checkbox from './Components/Checkbox/Checkbox';
 
 class App extends Component {
   render() {
@@ -13,6 +14,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <Checkbox label='Test' onChange={()=>{console.log('Changed')}} />
       </div>
     );
   }
